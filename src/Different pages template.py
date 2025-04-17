d528c764ca1289db2f49fe661a793118e3a3790a
JessiHudgins <9jbear@gmail.com>
2025-04-17 10:00:27 -0400

    Create Different pages template.py
    This is the set up if we want to have multiple pages.

---
+++ src/Different pages template.py
@@ -0,0 +1,6 @@
+if data_directory:
+    #First load the dataset
+    df, category_index = load_labeled_dataset(r"/Users/jessihudgins/Desktop/Hitachi_1-main/dataset/20_newsgroup")
+    if page == "Data Exploration":
+        #Take the normal code and just put it under this section
+    elif page == "Article Categorization":
